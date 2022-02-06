4fe92b4d3a8ed9cd495082f57c81532fc13300da
NiklasNeudorfer <nikitrash93@gmail.com>
2022-02-06 11:41:59 +0100

    added "Usage" class with repository with panache

---
+++ backend/src/main/java/at/htl/entity/Position.java
@@ -0,0 +1,5 @@
+package at.htl.entity;
+
+public enum Position {
+    CEILING, NORTH_WALL, SOUTH_WALL, WEST_WALL, EAST_WALL, FLOOR
+}
--- backend/src/main/java/at/htl/entity/Usage.java
+++ backend/src/main/java/at/htl/entity/Usage.java
@@ -1,5 +1,23 @@
 package at.htl.entity;
 
-public class Usage {
-    private String usage;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Usage extends PanacheEntity {
+
+    public String usage;
+
+    @OneToOne
+    public Device device;
+
+    @OneToOne
+    public Room room;
+
+    @Enumerated
+    public Position position;
+
 }
